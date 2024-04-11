97ddab7a933d073ba2c82b828df76f1792b8d6d8
HossamMahmoudKhedr <nhem318@gmail.com>
2024-04-11 18:25:23 +0200

    add images, icons and prepare signup page

---
+++ frontend/src/utils/icons.js
@@ -0,0 +1,7 @@
+import { ReactComponent as Leafs } from '../assets/icons/Vector.svg';
+import { ReactComponent as Stars } from '../assets/icons/noun-star-1138021 1.svg';
+
+const icons = {
+	leafs: <Leafs />,
+	stars: <Stars />,
+};
